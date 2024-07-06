28861d6e0abf0a6d39bea4f90cf9756bab34071e
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-06 22:28:19 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -30,9 +30,9 @@ void generateSoundex(const char *name, char *soundex) {
         }
     }
 
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
-    }
+   // while (sIndex < 4) {
+     //   soundex[sIndex++] = '0';
+    //}
 
     soundex[4] = '\0';
 }
