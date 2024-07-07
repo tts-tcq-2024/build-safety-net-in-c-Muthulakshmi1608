673be0315f6d517e0f9178620356f6bf5ce065a7
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-07 14:30:23 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,67 +1,74 @@
-
 #ifndef SOUNDEX_H
-
 #define SOUNDEX_H
- 
+
 #include "Soundex.h"
-
+#include <stdio.h>
 #include <ctype.h>
-
 #include <string.h>
- 
-char getSoundexCode(char c) {
 
-    c = toupper(c);
+int case1() { return 1; }
+int case2() { return 2; }
+int case3() { return 3; }
+int case5() { return 5; }
+int defaultCase() { return 0; }
 
-    switch (c) {
+typedef struct {
+    char value[10];
+    int (*func)();
+} CaseEntry;
 
-        case 'B': case 'F': case 'P': case 'V': return '1';
+int getSoundexCode(char letter) {
+    static CaseEntry cases[] = {
+        {{'B','F','P','V', '\0'}, case1},
+        {{'C','G','J','K','Q','S','X','Z', '\0'}, case2},
+        {{'D','T', '\0'}, case3},
+        {{'M','N', '\0'}, case5},
+        {{'A','E','I','O','U','H','W','Y', '\0'}, defaultCase}
+    };
 
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-
-        case 'D': case 'T': return '3';
-
-        case 'L': return '4';
-
-        case 'M': case 'N': return '5';
-
-        case 'R': return '6';
-
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-
-    }
-
-}
- 
-void generateSoundex(const char *name, char *soundex) {
-
-    int len = strlen(name);
-
-    soundex[0] = toupper(name[0]);
-
-    int sIndex = 1;
- 
-    for (int i = 1; i < len && sIndex < 4; i++) {
-
-        char code = getSoundexCode(name[i]);
-
-        if (code != '0' && code != soundex[sIndex - 1]) {
-
-            soundex[sIndex++] = code;
-
+    for (int i = 0; i < 5; i++) {
+        int j = 0;
+        while (cases[i].value[j] != '\0') {
+            if (cases[i].value[j] == letter) {
+                return cases[i].func();
+            }
+            j++;
         }
-
     }
- 
-   // while (sIndex < 4) {
 
-     //   soundex[sIndex++] = '0';
+    if (letter == 'L') {
+        return 4;
+    }
+    if (letter == 'R') {
+        return 6;
+    }
 
-    //}
- 
-    soundex[4] = '\0';
-
+    return defaultCase();
 }
- 
-#endif // SOUNDEX_H
- 
+
+void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
+    soundex[0] = toupper(name[0]);
+    int sIndex = 1;
+
+    for (int i = 1; i < len && sIndex < 4; i++) {
+        int code = getSoundexCode(toupper(name[i]));
+        if (code != 0 && code != soundex[sIndex - 1]) {
+            soundex[sIndex++] = '0' + code;  // Convert int to char
+        }
+    }
+
+    while (sIndex < 4) {
+        soundex[sIndex++] = '0';
+    }
+
+    soundex[4] = '\0';
+}
+
+int main() {
+    char value[10] = "Robert";
+    char soundex[10];
+    generateSoundex(value, soundex);
+    printf("Soundex: %s\n", soundex);
+    return 0;
+}
