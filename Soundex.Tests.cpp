c71690b8706ef1a73c852891c04b2d9bf961b12d
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-13 13:23:55 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,12 +4,14 @@
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
  char soundex[5];
-  generateSoundex("Robert", soundex);
-  generateSoundex("Rubin", soundex);
+ generateSoundex("Robert", soundex);
+ generateSoundex("Rubin", soundex);
  generateSoundex("Honeyman", soundex);
  generateSoundex("Rupert", soundex);
  generateSoundex("Ashcraft", soundex);
- 
+ generateSoundex("Tymczak", soundex);
+ generateSoundex("Pfister", soundex);
+
   //ASSERT_EQ(soundex,"R163");
 }
 
