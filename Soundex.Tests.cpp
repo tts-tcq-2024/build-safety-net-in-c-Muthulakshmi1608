d6011ff9adbcda6ea778c54dcf872278cc2b2049
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-13 12:26:58 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,15 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
+ char soundex[5];
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+ //AAA
+  generateSoundex("Robert", soundex);
+  //ASSERT_EQ(soundex,"R163");
+}
 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
-  char soundex[5];
-  generateSoundex("Robert", soundex);
-  //ASSERT_EQ(soundex,"R163");
+  //char soundex[5];
+  generateSoundex("Rubin", soundex);
+  //ASSERT_EQ(soundex,"R150");
 }
